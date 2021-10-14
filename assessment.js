25933223a4e1ca402ddcf357b25a9a90d4ec8650
newtestgo <92199074+newtestgo@users.noreply.github.com>
2021-10-14 14:55:02 +0900

    #2　に対する変更を行いました

--- assessment.js
+++ assessment.js
@@ -57,7 +57,7 @@ function removeAllChildren(element) {
     }
 }
 
-const answers = [
+const answer = [
   '{userName}のいいところは声です。{userName}の特徴的な声は皆を惹きつけ、心に残ります。',
   '{userName}のいいところはまなざしです。{userName}に見つめられた人は、気になって仕方がないでしょう。',
   '{userName}のいいところは情熱です。{userName}の情熱に周りの人は感化されます。',
